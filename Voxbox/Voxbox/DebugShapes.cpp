bbfb17b00ed7699693e64b0b5e5fd915dadfe406
Jaddori <ohlsson.niclas@gmail.com>
2017-08-16 03:33:58 +0200

    Voxels - Fixed a semantic error in Debug Shapes.

--- Voxbox/Voxbox/DebugShapes.cpp
+++ Voxbox/Voxbox/DebugShapes.cpp
@@ -164,25 +164,35 @@ void DebugShapes::unload()
 
 void DebugShapes::render( const glm::mat4& projectionMatrix, const glm::mat4& viewMatrix )
 {
-	const int NUM_LINES = lines.getRead().getSize();
-	const int NUM_SPHERES = spheres.getRead().getSize();
-	const int NUM_AABB = AABBs.getRead().getSize();
-	const int NUM_OBB = OBBs.getRead().getSize();
+	const int LINE_COUNT = lines.getRead().getSize();
+	const int SPHERE_COUNT = spheres.getRead().getSize();
+	const int AABB_COUNT = AABBs.getRead().getSize();
+	const int OBB_COUNT = OBBs.getRead().getSize();
 
-	if( NUM_LINES > 0 )
+	if( LINE_COUNT > 0 )
 	{
-		glBindVertexArray( lineVAO );
-		glBindBuffer( GL_ARRAY_BUFFER, lineVBO );
-		glBufferSubData( GL_ARRAY_BUFFER, 0, sizeof(DebugLine)*NUM_LINES, lines.getRead().getData() );
-
 		lineShader.bind();
 		lineShader.setMat4( lineProjectionMatrixLocation, projectionMatrix );
 		lineShader.setMat4( lineViewMatrixLocation, viewMatrix );
 
-		glDrawArrays( GL_POINTS, 0, NUM_LINES );
+		glBindVertexArray( lineVAO );
+		glBindBuffer( GL_ARRAY_BUFFER, lineVBO );
+
+		int offset = 0;
+		while( offset < LINE_COUNT )
+		{
+			int count = LINE_COUNT - offset;
+			if( count > DEBUG_SHAPES_MAX_LINES )
+				count = DEBUG_SHAPES_MAX_LINES;
+
+			glBufferSubData( GL_ARRAY_BUFFER, 0, sizeof(DebugLine)*count, lines.getRead().getData() + offset );
+			glDrawArrays( GL_POINTS, 0, count );
+
+			offset += count;
+		}
 	}
 
-	if( NUM_SPHERES > 0 )
+	/*if( NUM_SPHERES > 0 )
 	{
 		glBindVertexArray( sphereVAO );
 		glBindBuffer( GL_ARRAY_BUFFER, sphereVBO );
@@ -193,9 +203,32 @@ void DebugShapes::render( const glm::mat4& projectionMatrix, const glm::mat4& vi
 		sphereShader.setMat4( sphereViewMatrixLocation, viewMatrix );
 
 		glDrawArrays( GL_POINTS, 0, NUM_SPHERES );
+	}*/
+
+	if( SPHERE_COUNT > 0 )
+	{
+		sphereShader.bind();
+		sphereShader.setMat4( sphereProjectionMatrixLocation, projectionMatrix );
+		sphereShader.setMat4( sphereViewMatrixLocation, viewMatrix );
+
+		glBindVertexArray( sphereVAO );
+		glBindBuffer( GL_ARRAY_BUFFER, sphereVBO );
+
+		int offset = 0;
+		while( offset < SPHERE_COUNT )
+		{
+			int count = SPHERE_COUNT - offset;
+			if( count > DEBUG_SHAPES_MAX_SPHERES )
+				count = DEBUG_SHAPES_MAX_SPHERES;
+
+			glBufferSubData( GL_ARRAY_BUFFER, 0, sizeof(DebugSphere)*count, spheres.getRead().getData() + offset );
+			glDrawArrays( GL_POINTS, 0, count );
+
+			offset += count;
+		}
 	}
 
-	if( NUM_AABB > 0 )
+	/*if( NUM_AABB > 0 )
 	{
 		glBindVertexArray( aabbVAO );
 		glBindBuffer( GL_ARRAY_BUFFER, aabbVBO );
@@ -206,9 +239,32 @@ void DebugShapes::render( const glm::mat4& projectionMatrix, const glm::mat4& vi
 		aabbShader.setMat4( aabbViewMatrixLocation, viewMatrix );
 
 		glDrawArrays( GL_POINTS, 0, NUM_AABB );
+	}*/
+
+	if( AABB_COUNT > 0 )
+	{
+		aabbShader.bind();
+		aabbShader.setMat4( aabbProjectionMatrixLocation, projectionMatrix );
+		aabbShader.setMat4( aabbViewMatrixLocation, viewMatrix );
+
+		glBindVertexArray( aabbVAO );
+		glBindBuffer( GL_ARRAY_BUFFER, aabbVBO );
+
+		int offset = 0;
+		while( offset < AABB_COUNT )
+		{
+			int count = AABB_COUNT - offset;
+			if( count > DEBUG_SHAPES_MAX_AABB )
+				count = DEBUG_SHAPES_MAX_AABB;
+
+			glBufferSubData( GL_ARRAY_BUFFER, 0, sizeof(DebugAABB)*count, AABBs.getRead().getData() + offset );
+			glDrawArrays( GL_POINTS, 0, count );
+
+			offset += count;
+		}
 	}
 
-	if( NUM_OBB > 0 )
+	/*if( NUM_OBB > 0 )
 	{
 		glBindVertexArray( obbVAO );
 		glBindBuffer( GL_ARRAY_BUFFER, obbVBO );
@@ -219,6 +275,29 @@ void DebugShapes::render( const glm::mat4& projectionMatrix, const glm::mat4& vi
 		obbShader.setMat4( obbViewMatrixLocation, viewMatrix );
 
 		glDrawArrays( GL_POINTS, 0, NUM_OBB );
+	}*/
+
+	if( OBB_COUNT > 0 )
+	{
+		obbShader.bind();
+		obbShader.setMat4( obbProjectionMatrixLocation, projectionMatrix );
+		obbShader.setMat4( obbViewMatrixLocation, viewMatrix );
+
+		glBindVertexArray( obbVAO );
+		glBindBuffer( GL_ARRAY_BUFFER, obbVBO );
+
+		int offset = 0;
+		while( offset < OBB_COUNT )
+		{
+			int count = OBB_COUNT - offset;
+			if( count > DEBUG_SHAPES_MAX_OBB )
+				count = DEBUG_SHAPES_MAX_OBB;
+
+			glBufferSubData( GL_ARRAY_BUFFER, 0, sizeof(DebugOBB)*count, OBBs.getRead().getData() + offset );
+			glDrawArrays( GL_POINTS, 0, count );
+
+			offset += count;
+		}
 	}
 
 	// reset
