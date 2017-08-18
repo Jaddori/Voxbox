bc967bec8b73e5d31c3a8e0157302ac64518d269
Jaddori <ohlsson.niclas@gmail.com>
2017-08-18 03:21:10 +0200

    Debug - Fixed the recently entered commands functionality.

--- Voxbox/build/assets/scripts/console.lua
+++ Voxbox/build/assets/scripts/console.lua
@@ -37,8 +37,12 @@ function console:load()
 	self.input.text = ""
 	self.input.caretVisible = true
 	self.input.caretElapsed = CONSOLE_INPUT_CARET_FLASH_TIME
-	self.input.commands = { "a", "b", "c" }
 	self.input.currentCommand = 0
+	
+	self.input.commands = {}
+	for i=1, CONSOLE_INPUT_MAX_COMMANDS do
+		self.input.commands[i] = ""
+	end
 end
 
 function console:unload()
@@ -82,28 +86,12 @@ function console:update()
 					end
 				end
 				
-				-- check if entered command is already in list of remembered commands
-				local newCommand = true
-				for i=1, #self.input.commands do
-					if self.input.commands[i] == self.input.text then
-						newCommand = false
-						break
-					end
+				-- add enter command to list of remembered commands
+				for i=1, CONSOLE_INPUT_MAX_COMMANDS-1 do
+					self.input.commands[i] = self.input.commands[i+1]
 				end
 				
-				if newCommand then
-					-- add enter command to list of remembered commands
-					if #self.input.commands >= CONSOLE_INPUT_MAX_COMMANDS then
-						for i=1, CONSOLE_INPUT_MAX_COMMANDS-1 do
-							self.input.commands[i] = self.input.commands[i+1]
-						end
-						
-						self.input.commands[CONSOLE_INPUT_MAX_COMMANDS] = self.input.text
-					else
-						self.input.commands[#self.input.commands+1] = self.input.text
-					end
-				end
-				
+				self.input.commands[CONSOLE_INPUT_MAX_COMMANDS] = self.input.text
 				self.input.currentCommand = 0
 				
 				-- reset input text
@@ -115,7 +103,7 @@ function console:update()
 		if Input.keyRepeated( Keys.Up ) then
 			self.input.currentCommand = self.input.currentCommand - 1
 			if self.input.currentCommand <= 0 then
-				self.input.currentCommand = #self.input.commands
+				self.input.currentCommand = CONSOLE_INPUT_MAX_COMMANDS
 			end
 			
 			self.input.text = self.input.commands[self.input.currentCommand]
@@ -124,7 +112,7 @@ function console:update()
 		-- get down arrow
 		if Input.keyRepeated( Keys.Down ) then
 			self.input.currentCommand = self.input.currentCommand + 1
-			if self.input.currentCommand > #self.input.commands then
+			if self.input.currentCommand > CONSOLE_INPUT_MAX_COMMANDS then
 				self.input.currentCommand = 1
 			end
 			
